56e84c24f9a0117e35094fe0e38e7ddf24e8fc15
jungwinter <wintermy201@gmail.com>
2020-03-02 00:36:07 +0900

    Prepare to use typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,10 @@
-module.exports = {}
+module.exports = {
+    plugins: [
+        {
+            resolve: `gatsby-plugin-typography`,
+            opptions: {
+                pathToConfigModule: `src/utils/typography`,
+            },
+        },
+    ],
+}
