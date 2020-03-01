7e76fb3b0bb8f8528a2b8342f935129b52f237ff
jungwinter <wintermy201@gmail.com>
2020-03-02 01:09:51 +0900

    Disable link style in navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,14 +3,14 @@ import { Link } from "gatsby"
 
 const ListLink = props => (
     <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-        <Link to={props.to}>{props.children}</Link>
+        <Link to={props.to} style={{ textDecoration: `none`, color: `rgba(0, 0, 0, 0.8)` }}>{props.children}</Link>
     </li>
 )
 
 export default ({ children }) => (
     <div style={{ margin: `3rem auto`, maxWidth: 600, padding: `0 1rem` }}>
         <header style={{ marginBottom: `1.5rem` }}>
-            <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+            <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`, textDecoration: `none`, color: `rgba(0, 0, 0, 0.8)` }}>
                 <h3 style={{ display: `inline` }}>Cold Storage</h3>
             </Link>
             <ul style={{ listStyle: `none`, float: `right` }}>
