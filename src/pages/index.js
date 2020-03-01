212eceeb44e32b72864af3aeb839f934030ef270
jungwinter <wintermy201@gmail.com>
2020-03-02 00:11:19 +0900

    Remove random image

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,5 @@ export default () => (
         <Link to="/about">About</Link>
         <Header headerText="안녕! 나는 winter!" />
         <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/400x200" />
     </div>
 )
