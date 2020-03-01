87ef31546b03861a370097549923dc9a2b8e21d1
jungwinter <wintermy201@gmail.com>
2020-03-02 00:36:23 +0900

    Apply typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
 
 export default () => (
-    <div style={{ color: `purple` }}>
-        <Link to="/about">About</Link>
-        <Header headerText="안녕! 나는 winter!" />
-        <p>What a world.</p>
-    </div>
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+    <p>
+      typography 플러그인을 적용해봤다. 근데 과연 CJK 지원이 제대로 될까?
+    </p>
+  </div>
 )
