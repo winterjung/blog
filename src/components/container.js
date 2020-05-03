3a60c50eb796dd973de07820a7faf02d1ed30686
winter jung <me@winterjung.dev>
2020-05-03 16:30:09 +0900

    Remove old resources

--- src/components/container.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import containerStyles from "./container.module.css"
-
-export default ({ children }) =>  (
-    <div className={containerStyles.container}>{children}</div>
-)
