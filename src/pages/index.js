bbae20b7d80a98476b9decc534a796f95329f1e5
winter jung <me@winterjung.dev>
2020-05-04 20:35:48 +0900

    Link to post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
@@ -17,18 +17,25 @@ export default ({ data }) => (
             <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
             {data.allMarkdownRemark.edges.map(({ node }) => (
                 <div key={node.id}>
-                    <h3
+                    <Link
+                        to={node.fields.slug}
                         css={css`
-                            margin-bottom: ${rhythm(1 / 4)};
+                            text-decoration: none;
+                            color: inherit;
                         `}>
-                        {node.frontmatter.title}{" "}
-                        <span
+                        <h3
                             css={css`
-                                color: #bbb;
+                                margin-bottom: ${rhythm(1 / 4)};
                             `}>
-                            - {node.frontmatter.date}
-                        </span>
-                    </h3>
+                            {node.frontmatter.title}{" "}
+                            <span
+                                css={css`
+                                    color: #bbb;
+                                `}>
+                                - {node.frontmatter.date}
+                            </span>
+                        </h3>
+                    </Link>
                     <p>{node.excerpt}</p>
                 </div>
             ))}
@@ -46,6 +53,9 @@ export const query = graphql`
                     frontmatter {
                         title
                         date(formatString: "YYYY-MM-DD")
+                    }
+                    fields {
+                        slug
                     }
                     excerpt
                 }
