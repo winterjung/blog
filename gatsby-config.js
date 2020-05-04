5d417bbe23d02b681b48570637e71425c2f8a1bc
winter jung <me@winterjung.dev>
2020-05-04 18:07:48 +0900

    List posts with desc order by post date

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
                 path: `${__dirname}/src/`,
             },
         },
+        `gatsby-transformer-remark`,
         `gatsby-plugin-emotion`,
         {
             resolve: `gatsby-plugin-typography`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,55 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
-export default () => (
+export default ({ data }) => (
     <Layout>
-        <h1>블로그 제목 뭐라고 하지</h1>
         <div>
-            <img
-                src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-                alt="Group of pandas eating bamboo"
-            />
+            <h1
+                css={css`
+                    display: inline-block;
+                    border-bottom: 1px solid;
+                `}>
+                메인 페이지
+            </h1>
+            <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+                <div key={node.id}>
+                    <h3
+                        css={css`
+                            margin-bottom: ${rhythm(1 / 4)};
+                        `}>
+                        {node.frontmatter.title}{" "}
+                        <span
+                            css={css`
+                                color: #bbb;
+                            `}>
+                            - {node.frontmatter.date}
+                        </span>
+                    </h3>
+                    <p>{node.excerpt}</p>
+                </div>
+            ))}
         </div>
     </Layout>
 )
+
+export const query = graphql`
+    query {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+            totalCount
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        date(formatString: "YYYY-MM-DD")
+                    }
+                    excerpt
+                }
+            }
+        }
+    }
+`
