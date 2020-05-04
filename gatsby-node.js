597913443145d80639dfdeafa06550351d691ad3
winter jung <me@winterjung.dev>
2020-05-04 20:35:22 +0900

    Generate pages on build time

---
+++ gatsby-node.js
@@ -0,0 +1,41 @@
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({ node, getNode, trailingSlash: false }).replace(`posts/`, ``)
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        })
+    }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const result = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+            path: node.fields.slug,
+            component: path.resolve(`./src/templates/post.js`),
+            context: {
+                slug: node.fields.slug,
+            },
+        })
+    })
+}
