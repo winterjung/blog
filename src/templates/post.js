7faa6ffdfacaabdd10df04c831136eeb2225d55e
winter jung <me@winterjung.dev>
2020-05-04 22:10:17 +0900

    Indent

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,12 +4,14 @@ import Layout from "../components/layout"
 
 export default ({ data }) => {
     const post = data.markdownRemark
-    return <Layout>
-        <div>
-            <h1>{post.frontmatter.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </div>
-    </Layout>
+    return (
+        <Layout>
+            <div>
+                <h1>{post.frontmatter.title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            </div>
+        </Layout>
+    )
 }
 
 export const query = graphql`
