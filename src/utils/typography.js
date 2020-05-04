86e51c1e521e0a1d32c86e4758d1f0c0ffdb2d21
winter jung <me@winterjung.dev>
2020-05-05 00:02:59 +0900

    Use vanilla

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography"
-import kirkhamTheme from "typography-theme-kirkham"
 
-const typography = new Typography(kirkhamTheme)
+const typography = new Typography()
 
+typography.injectStyles()
 export const { scale, rhythm, options } = typography
 export default typography
