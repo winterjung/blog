9611c49b98a1823bfb111fbb8c3bffc20dff858d
winter jung <me@winterjung.dev>
2020-11-22 21:59:47 +0900

    Add seo component

---
+++ src/components/seo.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { useLocation } from "@reach/router"
+import { useStaticQuery, graphql } from "gatsby"
+
+export default function SEO({ title, description, image }) {
+    const { pathname } = useLocation()
+    const { site } = useStaticQuery(query)
+
+    const {
+        defaultTitle,
+        defaultDescription,
+        defaultImage,
+        baseUrl,
+        twitterUsername,
+    } = site.siteMetadata
+
+    const seo = {
+        title: title || defaultTitle,
+        description: description || defaultDescription,
+        url: new URL(pathname, baseUrl),
+        image: new URL(image || defaultImage, baseUrl),
+    }
+
+    return (
+        <Helmet>
+            <title>{seo.title}</title>
+            <link rel="canonical" href={seo.url} />
+            <meta name="description" content={seo.description} />
+            <meta name="image" content={seo.image} />
+
+            <meta property="og:title" content={seo.title} />
+            <meta property="og:url" content={seo.url} />
+            <meta property="og:description" content={seo.description} />
+            <meta property="og:image" content={seo.image} />
+            <meta property="og:type" content="article" />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={seo.title} />
+            <meta name="twitter:description" content={seo.description} />
+            <meta name="twitter:image" content={seo.image} />
+            <meta name="twitter:creator" content={twitterUsername} />
+        </Helmet>
+    )
+}
+
+export const query = graphql`
+    query {
+        site {
+            siteMetadata {
+            defaultTitle: title
+            defaultDescription: description
+            defaultImage: image
+            baseUrl: url
+            twitterUsername
+            }
+        }
+    }
+`
