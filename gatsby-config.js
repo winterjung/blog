bb76514e43746d1c489bfc2a6c9a7fbc5d510969
winter jung <me@winterjung.dev>
2020-11-22 21:59:35 +0900

    Add default site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
     siteMetadata: {
         title: `winterjung blog`,
+        description: `글 쓰는 소프트웨어 엔지니어`,
+        url: `https://blog.winterjung.dev`,
+        image: `favicon-256.png`,
+        twitterUsername: `@res_tin`,
     },
     plugins: [
         {
