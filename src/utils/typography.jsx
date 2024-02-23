cb133e0e4d010725d5bbac996a5197de3aa58c7c
winter jung <me@winterjung.dev>
2024-02-24 01:40:30 +0900

    Use pretendard for bold text

--- src/utils/typography.jsx
+++ src/utils/typography.jsx
@@ -1,6 +1,9 @@
 import Typography from "typography"
 
-const typography = new Typography()
+const typography = new Typography({
+    headerFontFamily: ["Pretendard", "Avenir Next", "Helvetica Neue"],
+    bodyFontFamily: ["Pretendard", "Avenir Next", "Helvetica Neue"],
+})
 
 typography.injectStyles()
 export const { scale, rhythm, options } = typography
